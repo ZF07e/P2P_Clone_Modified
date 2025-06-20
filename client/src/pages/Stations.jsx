c0a211ff4103d18bad82377f9ac1fe22eb909fce
brooks <bryllepastoral@gmail.com>
2025-06-20 16:24:56 +0800

    Stations page Finished

--- client/src/pages/Stations.jsx
+++ client/src/pages/Stations.jsx
@@ -7,11 +7,25 @@ function Stations() {
     <>
         <Header />
         
-        <h5 className='font-bold text-3xl'>Stations</h5>
+        <div className='flex items-center justify-between'>
+          <h5 className='font-bold text-4xl'>Stations</h5>
+          <div>
 
-        <div className='mt-16 grid grid-cols-1 gap-2'>
-            <StationCard />
-            <StationCard />
+            <div className='flex items-center border rounded'>
+              <img src="./src/assets/search.svg" alt="" className='h-5 w-5 mx-2 select-none' />
+              <input type="text" name="station_search" id="" placeholder='Search' className='w-md px-2 py-2 focus:outline-0 focus:bg-stone-100 rounded text-md' />
+            </div>
+
+          </div>
+        </div>
+        
+
+        <div className='mt-12 grid grid-cols-1 gap-2'>
+            <StationCard obj={{
+              name: "Terminal 1",
+              operator: "BBL",
+              address: "Calamba City, 4027 Laguna"
+            }} />
         </div>
     </>
   )
