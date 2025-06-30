81721b3da7212163c5cc344c480590c9d9a30a98
brooks <bryllepastoral@gmail.com>
2025-07-01 00:25:08 +0800

    server json connect

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const express = require("express");
 const app = express();
 const port = 3000;
+const terminals = require("./temp_data.json");
 const corsOption = {
     origin: "http://localhost:5173"
 }
@@ -11,3 +12,7 @@ app.use(cors(corsOption));
 app.listen(port, ()=>{
     console.log(`Server Is running on port ${port}`);
 })
+
+app.get('/api/terminals', (req, res)=>{
+    res.status(200).send(terminals)
+});
