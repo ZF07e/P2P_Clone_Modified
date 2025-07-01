ab4f9adabdf929913e10d1caec567773360d8caa
brooks <bryllepastoral@gmail.com>
2025-07-01 22:47:03 +0800

    Remove App.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Stations from './pages/Stations.jsx';
 import Home from './pages/Home.jsx';
 import Contact from './pages/Contact.jsx';
