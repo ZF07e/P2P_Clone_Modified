5bb3c2340e483b018298667b3518ae845fb500c2
brooks <bryllepastoral@gmail.com>
2025-07-09 20:31:18 +0800

    Redesign Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
 import "leaflet/dist/leaflet.css";
@@ -16,50 +17,103 @@ function SHOWLAT(){
     });
 
     console.log(position);
+}
 
+function customIcon(_iconsize){
+  return L.icon({
+    iconUrl: "./src/assets/mrk_.png",
+    iconSize: _iconsize,
+    iconAnchor: [22, 30],
+    popupAnchor: [-6, -24],
+  })
 }
 
 function Home() {
-  return (
+    const [marks, setMarks] = useState([]);
+
+    const Markings = async ()=>{
+        const response = await axios.get('http://localhost:3000/api/stations');
+        const stations = response.data;
+
+        setMarks(stations.map((obj, idx)=>{
+            console.log(obj)
+            return <Marker key={idx} position={obj.location} icon={customIcon()}>
+                <Popup>
+                    <div className='text-center w-full '>
+                        <p>{obj.terminalName}</p>
+                        <Link to={`stations/${obj.terminalName}`} 
+                              className='border px-3 py-2 rounded border-(--dark-color) !text-(--dark-color)
+                                         hover:bg-(--dark-color) hover:!text-(--primary-color) transition'>View Station</Link>
+                    </div>
+                </Popup>
+            </Marker> 
+        }));
+    }
+
+    useEffect(()=>{
+        Markings();
+    },[])
+
+    return (
     <>
         <Header active_nav={"Home"} />
-        <section className='h-dvh grid grid-cols-1 grid-rows-[1fr_auto_1fr] md:px-12'>
+        <section className='grid grid-rows-[auto_auto_1fr] gap-4 py-4'>
 
-            <div className='select-none mt-auto'>
-                <img src="./src/assets/bus-stop1.png" alt="" className='w-100 aspect-square mx-auto' />
+
+            <div className='bg-[url(src/assets/manila.jpg)] bg-center bg-cover py-32'>
+                <div className='flex items-center justify-center gap-2 bg-stone-900/50 py-2 text-stone-100'>   
+                    <img src="./src/assets/P2P-Logo-Light.png" alt="" className='aspect-square w-18' />
+                    <div>
+                        <h5 className='font-black text-4xl select-none'>P2P BUS PHILIPPINES.</h5>
+                        <p className='font-medium text-sm'>A premium point-to-point bus service in the Philippines</p>
+                    </div>            
+                </div>
+
             </div>
+                
+            <div className='px-4'>
+                <hr className='text-(--dark-color)' />
+                <div className='grid grid-cols-1 grid-rows-3 text-center mx-4 md:grid-rows-1 md:grid-cols-3 gap-4 mt-4 text-(--dark-color)'>
 
-            <div className='text-center'>
-                <h5 className='font-black text-4xl select-none text-(--dark-color)'>P2P BUS PHILIPPINES.</h5>
-                <p className='font-medium text-sm text-stone-800'>A premium point-to-point bus service in the Philippines</p>
-            </div>
+                    <div>
+                        <div>
+                            <img src="./src/assets/icon1.png" className='aspect-square w-20 mx-auto' alt="" />
+                            <h4 className='font-medium'>Faster, Non-Stop Commute</h4>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="./src/assets/icon2.png" className='aspect-square w-20 mx-auto' alt="" />
+                            <h4 className='font-medium'>Guaranteed Seating & Comfort</h4>
+                        </div>
+                    </div>
 
-            <div className='text-center'>
-                <div className='mt-4'>
-                    <select type="text" list='route_list' defaultValue={""} className='rounded py-2 border-1 focus:outline-0 w-[80%] sm:w-[70%] md:w-[50%] font-medium'>
-                        <option className='text-stone-300' value="" disabled >Select a route</option>
-                        <option value="">Terminal 1</option>
-                        <option value="">Terminal 2</option>
-                        <option value="">Terminal 3</option>
-                    </select>
+                    <div>
+                        <div>  
+                            <img src="./src/assets/icon3.png" className='aspect-square w-20 mx-auto' alt="" /> 
+                            <h4 className='font-medium'>Reliable Schedule</h4>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
         </section>
 
         <section className="bg-stone-100 h-dvh w-full p-8 gap-4 grid grid-cols-1 grid-rows-[1fr_auto_1fr] 
                             sm:grid-rows-[1fr_1fr] sm:grid-cols-[1fr_0.5fr]
                             md:gap-x-12 md:px-12 md:gap-y-0 
                             ">
-            <MapContainer center={[14.449548, 481.025391]} zoom={8} className='z-1 order-2 sm: sm:order-1 sm:row-span-2 aspect-3/2 my-auto rounded'>
+            <MapContainer center={[14.449548, 481.025391]} zoom={7} className='z-1 order-2 sm: sm:order-1 sm:row-span-2 aspect-3/2 my-auto rounded'>
                 <TileLayer attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'>
 
                 </TileLayer>
-
+                {marks}
                 <SHOWLAT />
             </MapContainer>
 
             <div className='order-1 md:order-2 mt-auto'>
-                <h5 className='font-black text-(--dark-color) text-2xl'>With over (n) Stations across Metro Manila and nearby Provinces</h5>
+                <h5 className='font-black text-(--dark-color) text-2xl'>With over {marks.length} Stations across Metro Manila and nearby Provinces</h5>
             </div>
 
             <div className='flex flex-col gap-4 order-3'>
